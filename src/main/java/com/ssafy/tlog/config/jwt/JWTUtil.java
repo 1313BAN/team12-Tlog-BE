82ccc68b7d1508125f8a26ae07cecc9397c2423c
yoownny <kelosy00@naver.com>
2025-05-13 01:34:31 +0900

    feat: jwt 의존성 버전 변경

--- src/main/java/com/ssafy/tlog/config/jwt/JWTUtil.java
+++ src/main/java/com/ssafy/tlog/config/jwt/JWTUtil.java
@@ -1,4 +1,27 @@
 package com.ssafy.tlog.config.jwt;
 
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JWTUtil {
+    private SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // user_id 추출
+    
+    // name 추출
+
+    // role 추출
+
+    // 만료 검증
+
+    // 토큰 생성
 }
