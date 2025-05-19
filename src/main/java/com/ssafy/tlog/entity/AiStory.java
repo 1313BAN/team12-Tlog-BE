28ecf3ffe916c00798559a994a373eb388fbc3eb
yoownny <kelosy00@naver.com>
2025-05-19 15:47:25 +0900

    refac: aiStroyId로 필드명 수정

--- src/main/java/com/ssafy/tlog/entity/AiStory.java
+++ src/main/java/com/ssafy/tlog/entity/AiStory.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 public class AiStory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int AiStoryId;
+    private int aiStoryId;
 
     private int tripId;
     private int userId;
