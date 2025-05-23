fb3a176780378d9ae1a083f83e621313e9af4d50
yoownny <kelosy00@naver.com>
2025-05-23 17:35:57 +0900

    feat: 도시 조회 (#20)

--- src/main/java/com/ssafy/tlog/entity/City.java
+++ src/main/java/com/ssafy/tlog/entity/City.java
@@ -1,14 +1,18 @@
 package com.ssafy.tlog.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class City {
     @Id
