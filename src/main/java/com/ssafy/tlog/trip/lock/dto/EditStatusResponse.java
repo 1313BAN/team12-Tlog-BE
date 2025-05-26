1e1e8942c530e5a018ceb9c163cb7ee3a0feebc2
yoownny <kelosy00@naver.com>
2025-05-27 02:43:48 +0900

    refac: islocked -> locked

--- src/main/java/com/ssafy/tlog/trip/lock/dto/EditStatusResponse.java
+++ src/main/java/com/ssafy/tlog/trip/lock/dto/EditStatusResponse.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EditStatusResponse {
-    private boolean isLocked;
+    private boolean locked;
     private Integer currentOwner;
 }
