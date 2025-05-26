870d7a2469427e90a278bd7baf410efffe071f3f
dalsu0222 <dalsu0222@sju.ac.kr>
2025-05-26 12:02:12 +0900

    feat: 여행 계획 디테일 조회 API 구현

--- src/main/java/com/ssafy/tlog/trip/plan/controller/TripPlanController.java
+++ src/main/java/com/ssafy/tlog/trip/plan/controller/TripPlanController.java
@@ -3,18 +3,15 @@ package com.ssafy.tlog.trip.plan.controller;
 import com.ssafy.tlog.common.response.ApiResponse;
 import com.ssafy.tlog.common.response.ResponseWrapper;
 import com.ssafy.tlog.config.security.CustomUserDetails;
+import com.ssafy.tlog.trip.plan.dto.TripPlanDetailResponseDto;
 import com.ssafy.tlog.trip.plan.dto.TripPlanRequestDto;
 import com.ssafy.tlog.trip.plan.dto.TripPlanResponseDto;
 import com.ssafy.tlog.trip.plan.service.TripPlanService;
-import com.ssafy.tlog.trip.record.dto.TripRecordResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,4 +28,14 @@ public class TripPlanController {
         TripPlanResponseDto responseDto = tripPlanService.createTripPlan(requestDto, userDetails.getUser());
         return ApiResponse.success(HttpStatus.OK, "여행 계획이 성공적으로 저장되었습니다.",responseDto);
     }
+
+    @GetMapping("/{tripId}/plan")
+    public ResponseEntity<ResponseWrapper<TripPlanDetailResponseDto>> getTripPlanDetail(
+            @PathVariable int tripId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        TripPlanDetailResponseDto responseDto = tripPlanService.getTripPlanDetail(tripId, userDetails.getUser());
+        return ApiResponse.success(HttpStatus.OK, "여행 계획 조회가 성공적으로 완료되었습니다.", responseDto);
+    }
+
 }
